962ffbc7a420bcd7a4a072773980cfcbb1bc2394
Veselin Velkov <vesselin60@gmail.com>
2023-07-19 16:46:04 +0300

    Play 19. Introducción a ConstraintLayout

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Layouts.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Layouts.kt
@@ -11,13 +11,40 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun LayoutPreview() {
 //    NewFrameLayout()
 
-    NewLinearLayout()
+//    NewLinearLayout()
+
+    NewConstraintLayout()
+}
+
+@Composable
+fun NewConstraintLayout() {
+    ConstraintLayout {
+        val (normA, normF, filledF, container) = createRefs()
+
+        Text(
+            text = "ANT Android Courses",
+            modifier = Modifier.constrainAs(normA) { top.linkTo(parent.top) })
+        Text(text = "Frogames",
+            modifier = Modifier.constrainAs(normF) { top.linkTo(normA.bottom) })
+        TextFro(modifier = Modifier.constrainAs(filledF) {
+            top.linkTo(normF.top)
+            start.linkTo(normF.end)
+        })
+        Row(modifier = Modifier.constrainAs(container){
+            top.linkTo(normF.bottom)
+        }) {
+            TextANT()
+            TextFro()
+        }
+    }
 }
 
 @Composable
@@ -47,13 +74,13 @@ fun NewLinearLayout() {
 }
 
 @Composable
-fun TextANT() {
-    Text(text = "ANT Android Courses", modifier = Modifier.background(Color.Cyan))
+fun TextANT(modifier: Modifier = Modifier) {
+    Text(text = "ANT Android Courses", modifier = modifier.background(Color.Cyan))
 }
 
 @Composable
-fun TextFro() {
-    Text(text = "Frogames", modifier = Modifier.background(Color.LightGray))
+fun TextFro(modifier: Modifier = Modifier) {
+    Text(text = "Frogames", modifier = modifier.background(Color.LightGray))
 }
 
 @Composable
