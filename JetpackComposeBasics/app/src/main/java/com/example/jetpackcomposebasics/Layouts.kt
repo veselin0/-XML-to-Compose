db9ae9ef2967724e172661bdbc48fa2adb68ab43
Veselin Velkov <vesselin60@gmail.com>
2023-07-19 16:07:37 +0300

    17. Row(LinearLayout horizontal)

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Layouts.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Layouts.kt
@@ -1,11 +1,15 @@
 package com.example.jetpackcomposebasics
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -18,10 +22,38 @@ private fun LayoutPreview() {
 
 @Composable
 fun NewLinearLayout() {
-    Column(horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Center) {
-        Text(text = "ANT Android Courses")
-        Text(text = "Frogames")
+//    Column(horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Center) {
+//        Text(text = "ANT Android Courses")
+//        Text(text = "Frogames")
+//
+//        Row {
+//            TextANT()
+//            TextFro()
+//        }
+//    }
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        TextFro()
+        Column {
+            TextANT()
+            TextANT()
+            TextFro()
+            Row {
+                TextANT()
+                TextFro()
+            }
+        }
     }
+}
+
+@Composable
+fun TextANT() {
+    Text(text = "ANT Android Courses", modifier = Modifier.background(Color.Cyan))
+}
+
+@Composable
+fun TextFro() {
+    Text(text = "Frogames", modifier = Modifier.background(Color.LightGray))
 }
 
 @Composable
