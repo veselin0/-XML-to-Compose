d59aedbad0118655a75f195a48c779f93cdf0877
Veselin Velkov <vesselin60@gmail.com>
2023-07-24 15:43:57 +0300

    Play 30. Diseños avanzados en Text

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Components.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Components.kt
@@ -1,13 +1,22 @@
 package com.example.jetpackcomposebasics
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
@@ -19,18 +28,45 @@ private fun ComponentsPreview() {
 
 @Composable
 fun NewTextView() {
-    Text(
-        text = "Jetpack Compose",
-        color = Color.Red,
-        fontSize = 28.sp,
-        fontStyle = FontStyle.Italic,
-        fontWeight = FontWeight.ExtraBold,
-        style = TextStyle(
-            shadow = Shadow(
-                color = Color.Black,
-                offset = Offset(2f, 2f),
-                blurRadius = 4f
+    Column {
+        Text(
+            text = "Jetpack Compose",
+            color = Color.Red,
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Black,
+                    offset = Offset(2f, 2f),
+                    blurRadius = 4f
+                )
             )
         )
-    )
+
+        Text(
+            text = "Jetpack Compose desde cero, migra tus vistas de Android XML. " +
+                    "Jetpack Compose desde cero, migra tus vistas de Android XML",
+            style = MaterialTheme.typography.bodyLarge,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Text(text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            ) { append("Batman") }
+            append(", Bruce Wayne")
+        })
+
+        Text(
+            text = "Diseños avanzados en Text.",
+            Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
 }
