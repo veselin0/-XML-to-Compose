d85d155e2a05e7370d4ab9fd4f6c58a08a7adf50
Veselin Velkov <vesselin60@gmail.com>
2023-08-01 15:56:52 +0300

    44. FloatingActionButton y Snackbar

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/SystemComplements.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/SystemComplements.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposebasics
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -10,6 +11,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -21,11 +23,23 @@ fun SystemComplementsPreview() {
 fun NewAppBars() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
-    Scaffold(scaffoldState = scaffoldState, topBar = {
-        TopAppBar(title = { Text(text = stringResource(id = R.string.app_name))})
-
-
-    }) {
-        Components(modifier = Modifier.padding(it))
-    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text(text = "Add") },
+                onClick = {
+                    scope.launch {
+                        scaffoldState
+                            .snackbarHostState
+                            .showSnackbar("Launch Fragment / Dialog")
+                    }
+                })
+        },
+        content = {
+            Components(modifier = Modifier.padding(it))
+        })
 }
