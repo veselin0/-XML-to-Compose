3bff9b357e9a06dd1f5275298d99ad73dbdf7bb3
Veselin Velkov <vesselin60@gmail.com>
2023-08-04 16:35:26 +0300

    48. Navigation Drawer avanzado

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/SystemComplements.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/SystemComplements.kt
@@ -1,14 +1,20 @@
 package com.example.jetpackcomposebasics
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -26,10 +32,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -69,6 +79,7 @@ fun NewAppBars() {
             )
         },
         drawerContent = {
+            var selectedItem by remember { mutableStateOf("Favorites") }
             val menuItems = mapOf<String, ImageVector>(
                 Pair("Favorites", Icons.Filled.Favorite),
                 Pair("Save", Icons.Filled.Send),
@@ -76,14 +87,33 @@ fun NewAppBars() {
                 Pair("Settings", Icons.Filled.Settings),
             )
 
+            Divider(modifier = Modifier.padding(top = 128.dp, bottom = 8.dp))
+
             menuItems.forEach {
-                Row(modifier = Modifier.clickable {
-                    scope.launch {
-                        scaffoldState.drawerState.close()
-                    }
-                }) {
-                    Icon(imageVector = it.value, contentDescription = it.key)
-                    Text(text = it.key)
+                Row(
+                    modifier = Modifier
+                        .height(56.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            selectedItem = it.key
+                            scope.launch {
+                                scaffoldState.drawerState.close()
+                            }
+
+                        }
+                        .padding(horizontal = 12.dp)
+                        .clip(RoundedCornerShape(28.dp))
+                        .background(if (selectedItem == it.key) Color.LightGray else Color.White),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = it.value,
+                        contentDescription = it.key,
+                        modifier = Modifier
+                            .size(56.dp)
+                            .padding(16.dp, end = 12.dp)
+                    )
+                    Text(text = it.key, style = MaterialTheme.typography.h6)
                 }
             }
         },
