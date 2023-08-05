a56578e77b1cd8b40467e497a49906b936c534c0
Veselin Velkov <vesselin60@gmail.com>
2023-08-05 15:40:19 +0300

    50. Principios responsivos Parte I

--- JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Modifiers.kt
+++ JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/Modifiers.kt
@@ -3,7 +3,9 @@ package com.example.jetpackcomposebasics
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,18 +16,51 @@ import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview(showBackground = true, showSystemUi = true, backgroundColor = 0xFFE1E1E1)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    backgroundColor = 0xFFFFFFFF,
+    device = Devices.NEXUS_7
+)
 @Composable
 private fun ModifierPreview() {
 //    NewSpacing()
 //    NewSize()
-    NewClick()
+//    NewClick()
+    NewResponsive()
+}
+
+@Composable
+fun NewResponsive() {
+    BoxWithConstraints() {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            if (minWidth > 600.dp) {
+                Row(modifier = Modifier.padding(8.dp)) {
+                    TextFro(modifier = Modifier.fillMaxWidth())
+                    TextANT()
+                }
+            } else {
+                Column(modifier = Modifier.padding(8.dp)) {
+                    TextFro(modifier = Modifier.fillMaxWidth())
+                    TextANT()
+                }
+            }
+
+        }
+    }
+
 }
 
 @Composable
