f441395b6fe77a48b7f73c181b31b633a7e2beb9
Veselin Velkov <vesselin60@gmail.com>
2023-08-08 15:28:00 +0300

    56. Mejorar aspecto de la vista individual

--- FilmsJC/app/src/main/java/com/example/filmsjc/MainActivity.kt
+++ FilmsJC/app/src/main/java/com/example/filmsjc/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.filmsjc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.filmsjc.ui.theme.FilmsJCTheme
 
@@ -26,7 +25,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
                     ListBasic(FakeDatabase.getAllFilms())
                 }
@@ -37,9 +36,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ListBasic(films: List<Film>) {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState()) ) {
-        films.forEach { Text(text = it.name)}
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        films.forEach { ItemListBasic(it.name) }
     }
+}
+
+@Composable
+fun ItemListBasic(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.common_padding_default)),
+        style = MaterialTheme.typography.h6
+    )
 }
 
 @Preview(showBackground = true)
