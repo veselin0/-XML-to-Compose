34289d7b5a2157c6a870d7eebc12d79bcb19e785
Veselin Velkov <vesselin60@gmail.com>
2023-08-08 15:10:25 +0300

    55. Lista simple

--- FilmsJC/app/src/main/java/com/example/filmsjc/MainActivity.kt
+++ FilmsJC/app/src/main/java/com/example/filmsjc/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.filmsjc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.filmsjc.ui.theme.FilmsJCTheme
 
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ListBasic(FakeDatabase.getAllFilms())
                 }
             }
         }
@@ -30,17 +36,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListBasic(films: List<Film>) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState()) ) {
+        films.forEach { Text(text = it.name)}
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FilmsJCTheme {
-        Greeting("Android")
+        ListBasic(FakeDatabase.getAllFilms())
+
     }
 }
