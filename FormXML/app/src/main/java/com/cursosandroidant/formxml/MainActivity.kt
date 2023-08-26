5d7812607bf44b8729d5913496ae66c84e413098
Veselin Velkov <vesselin60@gmail.com>
2023-08-26 16:59:17 +0300

    68. Diseño básico para reemplazar nuestro primer TextField

--- FormXML/app/src/main/java/com/cursosandroidant/formxml/MainActivity.kt
+++ FormXML/app/src/main/java/com/cursosandroidant/formxml/MainActivity.kt
@@ -8,7 +8,19 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatCheckedTextView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.cursosandroidant.formxml.databinding.ActivityMainBinding
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
@@ -30,63 +42,108 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        val countries = arrayOf("Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "España",
-            "Estados Unidos", "México", "Panamá", "Peru", "Uruguay")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,
-            countries)
-        
+
+        val countries = arrayOf(
+            "Argentina", "Bolivia", "Chile", "Colombia", "Ecuador", "España",
+            "Estados Unidos", "México", "Panamá", "Peru", "Uruguay"
+        )
+        val adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_dropdown_item,
+            countries
+        )
+
         binding.actvCountries.setAdapter(adapter)
         binding.actvCountries.setOnItemClickListener { _, view, _, _ ->
             binding.etPlaceBirth.requestFocus()
-            Toast.makeText(this, (view as AppCompatCheckedTextView).text.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                (view as AppCompatCheckedTextView).text.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
         }
-        
+
         binding.etDateBirth.setOnClickListener {
             val builder = MaterialDatePicker.Builder.datePicker()
             val picker = builder.build()
-            
+
             picker.addOnPositiveButtonClickListener { timeInMilliseconds ->
-                val dateStr = SimpleDateFormat("dd/MM/yyyy",
-                    Locale.getDefault()).apply {
+                val dateStr = SimpleDateFormat(
+                    "dd/MM/yyyy",
+                    Locale.getDefault()
+                ).apply {
                     timeZone = TimeZone.getTimeZone("UTC")
                 }.format(timeInMilliseconds)
-                
+
                 binding.etDateBirth.setText(dateStr)
             }
-            
+
             picker.show(supportFragmentManager, picker.toString())
         }
+
+        binding.etSurname.setContent {
+            MdcTheme {
+                EtSurname()
+            }
+        }
     }
-    
+
+    @Composable
+    private fun EtSurname() {
+        var textValue by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = textValue,
+            onValueChange = {textValue = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = dimensionResource(id = R.dimen.common_padding_default)),
+            label = {
+                Text(text = stringResource(id = R.string.hint_surname))
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = null)
+            }
+        )
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
-    
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_send){
+        if (item.itemId == R.id.action_send) {
             if (validFields()) {
-                val name: String = findViewById<TextInputEditText>(R.id.etName).text.toString().trim()
-                val surname = binding.etSurname.text.toString().trim()
+                val name: String =
+                    findViewById<TextInputEditText>(R.id.etName).text.toString().trim()
+//                val surname = binding.etSurname.text.toString().trim()
                 val height = binding.etHeight.text.toString().trim()
                 val dateBirth = binding.etDateBirth.text.toString().trim()
                 val country = binding.actvCountries.text.toString().trim()
                 val placeBirth = binding.etPlaceBirth.text.toString().trim()
                 val notes = binding.etNotes.text.toString().trim()
-                
+
                 val builder: AlertDialog.Builder = AlertDialog.Builder(this)
                 builder.setTitle(getString(R.string.dialog_title))
-                builder.setMessage(joinData(name, surname, height, dateBirth, country, placeBirth, notes))
+                builder.setMessage(
+                    joinData(
+                        name,
+                        "surname",
+                        height,
+                        dateBirth,
+                        country,
+                        placeBirth,
+                        notes
+                    )
+                )
                 builder.setPositiveButton(getString(R.string.dialog_ok)) { _, _ ->
                     with(binding) {
                         etName.text?.clear()
-                        etSurname.text?.clear()
+//                        etSurname.text?.clear()
                         etHeight.text?.clear()
                         etDateBirth.text?.clear()
                         actvCountries.text?.clear()
@@ -95,31 +152,31 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 builder.setNegativeButton(getString(R.string.dialog_cancel), null)
-                
+
                 val dialog: AlertDialog = builder.create()
                 dialog.show()
             }
         }
-        
+
         return super.onOptionsItemSelected(item)
     }
-    
+
     private fun joinData(vararg fields: String): String {
         var result = ""
-        
+
         fields.forEach { field ->
             if (field.isNotEmpty()) {
                 result += "$field\n"
             }
         }
-        
+
         return result
     }
-    
-    private fun validFields(): Boolean{
+
+    private fun validFields(): Boolean {
         var isValid = true
-        
-        if (binding.etHeight.text.isNullOrEmpty()){
+
+        if (binding.etHeight.text.isNullOrEmpty()) {
             binding.tilHeight.run {
                 error = getString(R.string.help_required)
                 requestFocus()
@@ -134,18 +191,18 @@ class MainActivity : AppCompatActivity() {
         } else {
             binding.tilHeight.error = null
         }
-        
-        if (binding.etSurname.text.isNullOrEmpty()){
-            binding.tilSurname.run {
-                error = getString(R.string.help_required)
-                requestFocus()
-            }
-            isValid = false
-        } else {
-            binding.tilSurname.error = null
-        }
-        
-        if (binding.etName.text.isNullOrEmpty()){
+
+//        if (binding.etSurname.text.isNullOrEmpty()) {
+//            binding.tilSurname.run {
+//                error = getString(R.string.help_required)
+//                requestFocus()
+//            }
+//            isValid = false
+//        } else {
+//            binding.tilSurname.error = null
+//        }
+
+        if (binding.etName.text.isNullOrEmpty()) {
             binding.tilName.run {
                 error = getString(R.string.help_required)
                 requestFocus()
@@ -154,7 +211,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             binding.tilName.error = null
         }
-        
+
         return isValid
     }
 }
