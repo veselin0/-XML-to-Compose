43949400c1b75e48da8f8dc955b138db73b1b908
Veselin Velkov <vesselin60@gmail.com>
2023-08-29 15:32:15 +0300

    77. Toolbar básica

--- FormJC/app/src/main/java/com/example/formjc/MainActivity.kt
+++ FormJC/app/src/main/java/com/example/formjc/MainActivity.kt
@@ -1,46 +1,83 @@
 package com.example.formjc
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.formjc.ui.theme.FormJCTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FormJCTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
+                    topBar = { ToolbarForm() },
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//                    contentColor = colorResource(id = R.color.purple_700),
+                    content = {
+                        CForm()
+                    }
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ToolbarForm() {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_send),
+                    contentDescription = stringResource(
+                        id = R.string.app_bar_action_send
+                    )
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = colorResource(id = R.color.purple_500),
+            titleContentColor = colorResource(id = R.color.white),
+            actionIconContentColor = colorResource(id = R.color.white)
+        )
     )
+}
+
+@Composable
+fun CForm() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FormJCTheme {
-        Greeting("Android")
+        CForm()
     }
 }
