fa4371aacbbf4792c7ead349c1551b6e31b17d96
Veselin Velkov <vesselin60@gmail.com>
2023-08-31 15:23:53 +0300

    No funciona Log.i tambien Toast

--- FormJC/app/src/main/java/com/example/formjc/MainActivity.kt
+++ FormJC/app/src/main/java/com/example/formjc/MainActivity.kt
@@ -1,28 +1,19 @@
 package com.example.formjc
 
 import android.annotation.SuppressLint
+import android.nfc.Tag
 import android.os.Bundle
-import android.widget.Toolbar
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.formjc.ui.components.ToolbarForm
 import com.example.formjc.ui.theme.FormJCTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +25,11 @@ class MainActivity : ComponentActivity() {
             FormJCTheme {
                 // A surface container using the 'background' color from the theme
                 Scaffold(
-                    topBar = { ToolbarForm() },
+                    topBar = {
+                        ToolbarForm {
+                            Toast.makeText(this, "Gocho", Toast.LENGTH_SHORT).show()
+                        }
+                    },
                     modifier = Modifier.fillMaxSize(),
                     content = {
                         CForm()
@@ -45,28 +40,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ToolbarForm() {
-    TopAppBar(
-        title = { Text(stringResource(id = R.string.app_name)) },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_send),
-                    contentDescription = stringResource(
-                        id = R.string.app_bar_action_send
-                    )
-                )
-            }
-        },
-        colors = TopAppBarDefaults.smallTopAppBarColors(
-            containerColor = colorResource(id = R.color.blue_500),
-            titleContentColor = colorResource(id = R.color.white),
-            actionIconContentColor = colorResource(id = R.color.white)
-        )
-    )
-}
 
 @Composable
 fun CForm() {
---
+++ FormJC/app/src/main/java/com/example/formjc/ui/components/Toolbar.kt
@@ -0,0 +1,36 @@
+package com.example.formjc.ui.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.formjc.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToolbarForm(onShowDialog: () -> Unit) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        actions = {
+            IconButton(onClick = { onShowDialog }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_send),
+                    contentDescription = stringResource(
+                        id = R.string.app_bar_action_send
+                    )
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = colorResource(id = R.color.blue_500),
+            titleContentColor = colorResource(id = R.color.white),
+            actionIconContentColor = colorResource(id = R.color.white)
+        )
+    )
+}
